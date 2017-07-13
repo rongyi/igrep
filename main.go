030743a1f254bdbd959d2969232570744b21787d
rongyi <rongyi3@wanda.cn>
2017-07-13 17:32:48 +0800

    Add ctrlc action

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
+
 import (
+	"github.com/nsf/termbox-go"
 )
 
 func main() {
+	err := termbox.Init()
 
+	if err != nil {
+		panic(err)
+	}
+	defer termbox.Close()
+
+mainloop:
+	for {
+		switch ev := termbox.PollEvent(); ev.Type {
+		case termbox.EventKey:
+			switch ev.Key {
+			case termbox.KeyCtrlC:
+				break mainloop
+			}
+		case termbox.EventError:
+			break mainloop
+		}
+	}
 }
